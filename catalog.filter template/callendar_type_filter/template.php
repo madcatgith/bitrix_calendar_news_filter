378172ee21bebb89ea841535662f44db3bbc814e
MaDCaT <wasd.exe@gmail.com>
2016-07-22 10:41:55 +0300

    clean version working on agrostory

---
+++ catalog.filter template/callendar_type_filter/template.php
@@ -0,0 +1,127 @@
+<?if(!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true)die();
+/** @var array $arParams */
+/** @var array $arResult */
+/** @global CMain $APPLICATION */
+/** @global CUser $USER */
+/** @global CDatabase $DB */
+/** @var CBitrixComponentTemplate $this */
+/** @var string $templateName */
+/** @var string $templateFile */
+/** @var string $templateFolder */
+/** @var string $componentPath */
+/** @var CBitrixComponent $component */
+$this->setFrameMode(true);
+?>
+<!--<form name="<?echo $arResult["FILTER_NAME"]."_form"?>" action="<?echo $arResult["FORM_ACTION"]?>" method="get">
+	<?foreach($arResult["ITEMS"] as $arItem):
+		if(array_key_exists("HIDDEN", $arItem)):
+			echo $arItem["INPUT"];
+		endif;
+	endforeach;?>
+	<table class="data-table" cellspacing="0" cellpadding="2">
+	<thead>
+		<tr>
+			<td colspan="2" align="center"><?=GetMessage("IBLOCK_FILTER_TITLE")?></td>
+		</tr>
+	</thead>
+	<tbody>
+		<?foreach($arResult["ITEMS"] as $arItem):?>
+			<?if(!array_key_exists("HIDDEN", $arItem)):?>
+				<tr>
+					<td valign="top"><?=$arItem["NAME"]?>:</td>
+					<td valign="top"><?=$arItem["INPUT"]?></td>
+				</tr>
+			<?endif?>
+		<?endforeach;?>
+	</tbody>
+	<tfoot>
+		<tr>
+			<td colspan="2">
+				<input type="submit" name="set_filter" value="<?=GetMessage("IBLOCK_SET_FILTER")?>" /><input type="hidden" name="set_filter" value="Y" />&nbsp;&nbsp;<input type="submit" name="del_filter" value="<?=GetMessage("IBLOCK_DEL_FILTER")?>" /></td>
+		</tr>
+	</tfoot>
+	</table>
+</form>-->
+
+
+
+<table id="calendar2">
+  <thead>
+    <tr><td>‹<td colspan="5"><td>›
+    <tr><td>Пн<td>Вт<td>Ср<td>Чт<td>Пт<td>Сб<td>Вс
+  <tbody>
+</table>
+
+<script>
+function Calendar2(id, year, month) {
+var Dlast = new Date(year,month+1,0).getDate(),
+    D = new Date(year,month,Dlast),
+    currmonth = month+1,
+    DNlast = new Date(D.getFullYear(),D.getMonth(),Dlast).getDay(),
+    DNfirst = new Date(D.getFullYear(),D.getMonth(),1).getDay(),
+    calendar = '<tr>',
+    month=["Январь","Февраль","Март","Апрель","Май","Июнь","Июль","Август","Сентябрь","Октябрь","Ноябрь","Декабрь"];
+    if (currmonth < 10){
+      currmonth='0'+currmonth;
+    }
+if (DNfirst != 0) {
+  for(var  i = 1; i < DNfirst; i++) calendar += '<td>';
+}else{
+  for(var  i = 0; i < 6; i++) calendar += '<td>';
+}
+for(var  i = 1; i <= Dlast; i++) {
+  if (i<10){
+     var start_filter_date='0'+i+'.'+currmonth+'.'+year;
+  }
+  else{
+    var start_filter_date=i+'.'+currmonth+'.'+year;
+  }
+
+  var end_filter_date=nextday(i,currmonth,year);
+
+  if (i == new Date().getDate() && D.getFullYear() == new Date().getFullYear() && D.getMonth() == new Date().getMonth()) {
+    calendar += '<td class="today">' +'<a href="?arrFilter_DATE_ACTIVE_FROM_1='+start_filter_date+'&arrFilter_DATE_ACTIVE_FROM_2='+end_filter_date+'&set_filter=Фильтр&set_filter=Y">' + i + '</a>';
+  }else{
+    calendar += '<td>' +'<a href="?arrFilter_DATE_ACTIVE_FROM_1='+start_filter_date+'&arrFilter_DATE_ACTIVE_FROM_2='+end_filter_date+'&set_filter=Фильтр&set_filter=Y">' + i + '</a>';
+  }
+  
+  if (new Date(D.getFullYear(),D.getMonth(),i).getDay() == 0) {
+    calendar += '<tr>';
+  }
+}
+for(var  i = DNlast; i < 7; i++) calendar += '<td>&nbsp;';
+document.querySelector('#'+id+' tbody').innerHTML = calendar;
+document.querySelector('#'+id+' thead td:nth-child(2)').innerHTML = month[D.getMonth()] +' '+ D.getFullYear();
+document.querySelector('#'+id+' thead td:nth-child(2)').dataset.month = D.getMonth();
+document.querySelector('#'+id+' thead td:nth-child(2)').dataset.year = D.getFullYear();
+if (document.querySelectorAll('#'+id+' tbody tr').length < 6) {  // чтобы при перелистывании месяцев не "подпрыгивала" вся страница, добавляется ряд пустых клеток. Итог: всегда 6 строк для цифр
+    document.querySelector('#'+id+' tbody').innerHTML += '<tr><td>&nbsp;<td>&nbsp;<td>&nbsp;<td>&nbsp;<td>&nbsp;<td>&nbsp;<td>&nbsp;';
+}
+}
+Calendar2("calendar2", new Date().getFullYear(), new Date().getMonth());
+// переключатель минус месяц
+document.querySelector('#calendar2 thead tr:nth-child(1) td:nth-child(1)').onclick = function() {
+  Calendar2("calendar2", document.querySelector('#calendar2 thead td:nth-child(2)').dataset.year, parseFloat(document.querySelector('#calendar2 thead td:nth-child(2)').dataset.month)-1);
+}
+// переключатель плюс месяц
+document.querySelector('#calendar2 thead tr:nth-child(1) td:nth-child(3)').onclick = function() {
+  Calendar2("calendar2", document.querySelector('#calendar2 thead td:nth-child(2)').dataset.year, parseFloat(document.querySelector('#calendar2 thead td:nth-child(2)').dataset.month)+1);
+}
+
+function nextday(day,month,year){
+  var y = parseInt(year);
+  var m = parseInt(month)-1;
+  var d = parseInt(day);
+  var tommorow = new Date(y,m,d+1);
+  d=tommorow.getDate();
+  m=tommorow.getMonth()+1;
+  y=tommorow.getFullYear();
+  if (d < 10){
+    d='0'+d;
+  }
+  if (m < 10){
+    m='0'+m;
+  }
+  return d+'.'+m+'.'+y;
+}
+</script>
